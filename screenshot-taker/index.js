bf3f1d924eaab5129c6999df416ffb9f13785797
xehapa <xehapa.dev@gmail.com>
2023-03-05 13:51:56 +0700

    second page

--- screenshot-taker/index.js
+++ screenshot-taker/index.js
@@ -2,7 +2,8 @@ const { chromium } = require('playwright')
 
 ;(async () => {
     const browser = await chromium.launch()
-    const page = await browser.newPage()
+    const context = await browser.newContext()
+    const page = await context.newPage()
     await page.goto('http://localhost:5500/sample-donut/chart.js/')
     await page.waitForTimeout(5000)
     await page.pdf({ path: 'test.pdf' })
