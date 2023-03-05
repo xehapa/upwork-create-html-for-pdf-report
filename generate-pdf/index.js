86b2bfaaa19ef5e8d7adb2766570fcc9ef4ac4bf
xehapa <xehapa.dev@gmail.com>
2023-03-06 05:11:23 +0700

    combine

--- generate-pdf/index.js
+++ generate-pdf/index.js
@@ -1,11 +1,22 @@
 const { chromium } = require('playwright')
 
 ;(async () => {
-    const browser = await chromium.launch()
+  const browser = await chromium.launch()
     const context = await browser.newContext()
     const page = await context.newPage()
-    await page.goto('http://localhost:5500/sample-donut/chart.js/')
-    await page.waitForTimeout(5000)
-    await page.pdf({ path: 'test.pdf' })
+    const urls = [
+      'http://localhost:5500/complete/page-1.html',
+      'http://localhost:5500/complete/page-2.html',
+      'http://localhost:5500/complete/page-3.html',
+      'http://localhost:5500/complete/page-4.html',
+      'http://localhost:5500/complete/page-5.html',
+      'http://localhost:5500/complete/page-6.html'
+    ]
+    
+    for (const url of urls) {
+      await page.goto(url);
+      await page.waitForTimeout(5000)
+      await page.pdf({path: `${new Date().toISOString()}.pdf`, printBackground: true})
+    }
     await browser.close()
-  })()
+})()
